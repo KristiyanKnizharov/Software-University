d6aa495e858506a8caa325698d84ffaad187f448
KristiyanKnizharov <49359681+KristiyanKnizharov@users.noreply.github.com>
2020-06-03 00:47:12 +0300

    Add files via upload

---
+++ C# Advanced/05. DefiningClasses/01_Basic/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace DefiningClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+
+            for (int i = 0; i < 3; i++)
+            {
+                doublyLinkedList.AddFirst(i);
+            }
+        }
+    }
+}
