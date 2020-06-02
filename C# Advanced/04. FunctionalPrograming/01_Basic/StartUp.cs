8937fdf1e95af8c2b2f2f5fbbb80749201dc1747
KristiyanKnizharov <49359681+KristiyanKnizharov@users.noreply.github.com>
2020-06-03 00:46:10 +0300

    Add files via upload

---
+++ C# Advanced/04. FunctionalPrograming/01_Basic/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FunctionalPrograming
+{
+    class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            Func<string, int, bool> func = new Func<string, int, bool>((name, num) =>
+            {
+                bool isEqualorLarger = false;
+                if (name.Length >= num)
+                {
+                    isEqualorLarger = true;
+                }
+                return isEqualorLarger;
+            });
+
+            int num = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            foreach (var item in names)
+            {
+                if (func(item, num))
+                {
+                    Console.WriteLine(item);
+                    return;
+                }
+            }
+        }
+    }
+}
