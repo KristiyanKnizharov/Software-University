4e93bae86e898eb473853dd054b9518646382790
KristiyanKnizharov <49359681+KristiyanKnizharov@users.noreply.github.com>
2020-06-03 00:37:03 +0300

    Add files via upload

---
+++ C# Advanced/03. Streams-Files/P02._LineNumbers/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace P02._LineNumbers
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("./text.txt");
+            
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int counterChars = 0;
+                int counterMarks = 0;
+                string currLines = lines[i];
+                foreach (char item in currLines)
+                {
+                    if (char.IsLetter(item))
+                    {
+                        counterChars++;
+                    }
+                    else if (char.IsPunctuation(item))
+                    {
+                        counterMarks++;
+                    }
+                }
+                Console.WriteLine($"Line {i + 1}:{currLines} ({counterChars}) ({counterMarks})");
+            }
+
+
+        }
+    }
+}
