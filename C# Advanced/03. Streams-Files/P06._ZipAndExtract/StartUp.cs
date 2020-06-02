7acfa6facb6e12fded4eed66a3908ccae1d39380
KristiyanKnizharov <49359681+KristiyanKnizharov@users.noreply.github.com>
2020-06-03 00:42:48 +0300

    Add files via upload

---
+++ C# Advanced/03. Streams-Files/P06._ZipAndExtract/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace P06._ZipAndExtract
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory(@"D:\SoftUniLocalDiskD", @"C:\Users\admin\Desktop\SoftUni\C#\Advance\Streams-Files.cs\myArchive.zip");
+            ZipFile.ExtractToDirectory(@"C:\Users\admin\Desktop\SoftUni\C#\Advance\Streams-Files.cs\myArchive.zip", @"C:\Users\admin\Desktop\copyMePNG");
+        }
+    }
+}
