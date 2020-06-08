0ab3252ee59278a166b397cea91a3a61167624b8
KristiyanKnizharov <49359681+KristiyanKnizharov@users.noreply.github.com>
2020-06-08 17:55:45 +0300

    Add files via upload

---
+++ C# OOP/02. Inheritance/P05_Restaurant/Food/Cake.cs
@@ -0,0 +1,20 @@
+﻿namespace Restaurant.Food
+{
+    public class Cake : Dessert
+    {
+        private const double GRAMS = 250;
+        private const double CALORIES = 1000;
+        private const decimal CAKE_PRICE = 5;
+        public Cake(string name, decimal price,
+            double grams)
+            : base(name, 0, 0, 0)
+        {
+
+        }
+        public override double Grams { get => GRAMS; }
+
+        public override double Calories { get => CALORIES; }
+
+        public override decimal Price { get => CAKE_PRICE; }
+    }
+}
