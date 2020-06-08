45028f45ed631de75e64d9e5a35e7065a4aef753
KristiyanKnizharov <49359681+KristiyanKnizharov@users.noreply.github.com>
2020-06-08 17:53:46 +0300

    Add files via upload

---
+++ C# OOP/02. Inheritance/P04_NeedForSpeed/Car/SportCar.cs
@@ -0,0 +1,10 @@
+﻿namespace NeedForSpeed.Car
+{
+    public class SportCar : Car
+    {
+        public SportCar(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+        }
+    }
+}
---
+++ C# OOP/02. Inheritance/P04_NeedForSpeed/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int hp = int.Parse(Console.ReadLine());
+            double fuel = double.Parse(Console.ReadLine());
+            double kilometers = double.Parse(Console.ReadLine());
+
+            Vehicle vehicle = new Vehicle(hp, fuel);
+            vehicle.Drive(kilometers);
+        }
+    }
+}
