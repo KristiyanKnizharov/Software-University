37484c9dabd3efe4acdae8389b4d6664b34a09aa
KristiyanKnizharov <49359681+KristiyanKnizharov@users.noreply.github.com>
2020-06-08 16:59:14 +0300

    Create Car.cs

---
+++ C# OOP/01. WorkingWithAbstraction/P01_RawData/Car.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_RawData
+{
+    public class Car
+    {
+        private readonly Tire[] tires;
+
+        public Car(string model,Engine engine
+            , Cargo cargo
+            , Tire[] tires)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Cargo = cargo;
+            this.tires = tires;
+        }
+        public Engine Engine { get; }
+        public Cargo Cargo  { get; }
+        public string Model { get; set; }
+
+        public IReadOnlyCollection<Tire> Tires
+        {
+            get
+            {
+                return this.tires;
+            }
+        }
+
+    }
+}
